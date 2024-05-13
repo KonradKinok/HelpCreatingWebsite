73086dd357c9ece6ea75c6d866c8df75640abe02
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-05-13 16:46:16 +0200

    12.05.2024

--- src/scripts/23.mjs
+++ src/scripts/23.mjs
@@ -9,7 +9,7 @@ const AXIOS_AUTHORIZATION =
   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YmI4OTQ0OTRjMWE3MDc2MTg2NDhiOTE2NGYzOTNjMiIsInN1YiI6IjVlZDdiZmY3ZTRiNTc2MDAyMDM3NjYzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kRGs0WRoomKwYXT7Mt8PNU2Zk6kAVasud5CyVVdf2mA';
 //Axios header - api key
 axios.defaults.headers.common['Authorization'] = AXIOS_AUTHORIZATION;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
 //DOM
 const searchButton = document.querySelector('button#search-button');
 const gallery = document.querySelector('ul#cards-list');
@@ -23,7 +23,6 @@ function getMostPopularMovies() {
   tmdb
     .getMostPopularMovies(1)
     .then(dataMovies => {
-      console.log(dataMovies);
       getTmdbConfiguration();
       renderMovies(dataMovies);
     })
@@ -36,64 +35,33 @@ function getTmdbConfiguration() {
   tmdb
     .getTmdbConfiguration()
     .then(dataMovies => {
-      console.log(dataMovies);
+      // console.log(dataMovies);
     })
     .catch(error => {
       //   Notify.failure(`${error}`, optionsNotify);
       console.log(error);
     });
 }
+const temp = tmdb.getUrlSizePoster('poster_path');
+// temp.
 function renderMovies(dataMovies) {
   gallery.innerHTML = null;
-  const markup = dataMovies.results
-    .map(({ id, original_title }) => {
-      //   return `<li>
-      //       <div class="card">
-      //         <div class="card-img" data-id="${id}"></div>
-      //         <div class="card-text">
-      //           <p class="card-text-title">Ghostbusters</p>
-      //           <p class="card-text-genre">Drama, Action | 2024</p>
-      //         </div>
-      //       </div>
-      //     </li>;`;
-      // return `  <div class="photo-card">
-      //   <a class="link-img" href="${largeImageURL}">
-      //     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  const filmsList = dataMovies.results
+    .map(({ id, original_title, poster_path, release_date, genre_ids }) => {
+      const urlSizePoster = tmdb.getUrlSizePoster(poster_path);
+      const url = urlSizePoster.find(obj => obj.name === 'w500');
+      const year = release_date.split('-')[0];
+      const genres = tmdb.getGenres(genre_ids);
 
-      //   <div class="info">
-      //     <div>
-      //       <p class="info-item">
-      //         <b>Likes</b>
-      //       </p>
-      //       <p class="info-item">${likes}</p>
-      //     </div>
-      //     <div>
-      //       <p class="info-item">
-      //         <b>Views</b>
-      //       </p>
-      //       <p class="info-item">${views}</p>
-      //     </div>
-      //     <div>
-      //       <p class="info-item">
-      //         <b>Comments</b>
-      //       </p>
-      //       <p class="info-item">${comments}</p>
-      //     </div>
-      //     <div>
-      //       <p class="info-item">
-      //         <b>Downloads</b>
-      //       </p>
-      //       <p class="info-item">${downloads}</p>
-      //     </div>
-      //   </div>
-      //   </a>
-      // </div>`;
       const listItem = document.createElement('li');
       const divCard = document.createElement('div');
       divCard.classList.add('card');
+      divCard.setAttribute('data-id', id);
 
       const divCardImg = document.createElement('div');
       divCardImg.classList.add('card-img');
+
+      divCardImg.style.backgroundImage = `url("${url.url}")`;
 
       const divCardText = document.createElement('div');
       divCardText.classList.add('card-text');
@@ -101,8 +69,10 @@ function renderMovies(dataMovies) {
       const pCardTextTitle = document.createElement('p');
       pCardTextTitle.classList.add('card-text-title');
       pCardTextTitle.textContent = original_title;
+
       const pCardTextGenre = document.createElement('p');
       pCardTextGenre.classList.add('card-text-genre');
+      pCardTextGenre.textContent = genres + ' | ' + year;
 
       divCardText.appendChild(pCardTextTitle);
       divCardText.appendChild(pCardTextGenre);
@@ -112,36 +82,35 @@ function renderMovies(dataMovies) {
 
       listItem.appendChild(divCard);
       gallery.appendChild(listItem);
-      console.log(id);
     })
     .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.insertAdjacentHTML('beforeend', filmsList);
 }
 
-function render() {
-  const listItem = document.createElement('li');
-  const divCard = document.createElement('div');
-  divCard.classList.add('card');
+// function render() {
+//   const listItem = document.createElement('li');
+//   const divCard = document.createElement('div');
+//   divCard.classList.add('card');
 
-  const divCardImg = document.createElement('div');
-  divCardImg.classList.add('card-img');
+//   const divCardImg = document.createElement('div');
+//   divCardImg.classList.add('card-img');
+// sectionOneBackground.style.backgroundImage = "url('images/original.jpg')";
+//   const divCardText = document.createElement('div');
+//   divCardText.classList.add('card-text');
 
-  const divCardText = document.createElement('div');
-  divCardText.classList.add('card-text');
+//   const pCardTextTitle = document.createElement('p');
+//   pCardTextTitle.classList.add('card-text-title');
 
-  const pCardTextTitle = document.createElement('p');
-  pCardTextTitle.classList.add('card-text-title');
+//   const pCardTextGenre = document.createElement('p');
+//   pCardTextGenre.classList.add('card-text-genre');
 
-  const pCardTextGenre = document.createElement('p');
-  pCardTextGenre.classList.add('card-text-genre');
+//   divCardText.appendChild(pCardTextTitle);
+//   divCardText.appendChild(pCardTextGenre);
 
-  divCardText.appendChild(pCardTextTitle);
-  divCardText.appendChild(pCardTextGenre);
+//   divCard.appendChild(divCardImg);
+//   divCard.appendChild(divCardText);
 
-  divCard.appendChild(divCardImg);
-  divCard.appendChild(divCardText);
-
-  listItem.appendChild(divCard);
-  gallery.appendChild(listItem);
-}
+//   listItem.appendChild(divCard);
+//   gallery.appendChild(listItem);
+// }
--- src/scripts/23a.mjs
+++ src/scripts/23a.mjs
@@ -25,8 +25,28 @@ export async function getTmdbConfiguration() {
   return response.data;
 }
 
-function getUrlSizePoster(size) {
-  let sizeString;
+// async function getTmdbGenres() {
+//   const searchParams = new URLSearchParams({
+//     language: 'en-US',
+//   });
+//   const url = `https://api.themoviedb.org/3/genre/movie/list?${searchParams}`;
+//   const response = await axios.get(url);
+//   return response.data;
+// }
+
+// export function getGenres() {
+//   try {
+//     const TAB_GENRIES = getTmdbGenres();
+//     // console.log(genres);
+//     return TAB_GENRIES;
+//   } catch (error) {
+//     // Notify.failure(`${error}`, optionsNotify);
+//     console.log(error);
+//     throw error; // Rzucamy błąd, aby móc go obsłużyć dalej, jeśli to konieczne
+//   }
+// }
+export function getUrlSizePoster(posterPath) {
+  const url = 'https://image.tmdb.org/t/p/';
   const poster_sizes = [
     'w92',
     'w154',
@@ -36,6 +56,98 @@ function getUrlSizePoster(size) {
     'w780',
     'original',
   ];
+  const postersUrlsObject = poster_sizes.map(size => {
+    return {
+      name: size,
+      url: url + size + posterPath,
+    };
+  });
+  return postersUrlsObject;
+}
 
-  const url = 'https://image.tmdb.org/t/p/';
+export function getGenres(genre_ids) {
+  const genres = [
+    {
+      id: 28,
+      name: 'Action',
+    },
+    {
+      id: 12,
+      name: 'Adventure',
+    },
+    {
+      id: 16,
+      name: 'Animation',
+    },
+    {
+      id: 35,
+      name: 'Comedy',
+    },
+    {
+      id: 80,
+      name: 'Crime',
+    },
+    {
+      id: 99,
+      name: 'Documentary',
+    },
+    {
+      id: 18,
+      name: 'Drama',
+    },
+    {
+      id: 10751,
+      name: 'Family',
+    },
+    {
+      id: 14,
+      name: 'Fantasy',
+    },
+    {
+      id: 36,
+      name: 'History',
+    },
+    {
+      id: 27,
+      name: 'Horror',
+    },
+    {
+      id: 10402,
+      name: 'Music',
+    },
+    {
+      id: 9648,
+      name: 'Mystery',
+    },
+    {
+      id: 10749,
+      name: 'Romance',
+    },
+    {
+      id: 878,
+      name: 'Science Fiction',
+    },
+    {
+      id: 10770,
+      name: 'TV Movie',
+    },
+    {
+      id: 53,
+      name: 'Thriller',
+    },
+    {
+      id: 10752,
+      name: 'War',
+    },
+    {
+      id: 37,
+      name: 'Western',
+    },
+  ];
+  const matchingGenres = genres
+    .filter(genre => genre_ids.includes(genre.id))
+    .map(genre => genre.name);
+
+  const genreNames = matchingGenres.join(', ');
+  return genreNames;
 }
