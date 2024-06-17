6071728f8be7bec92bbefeec9d3d8e06fd0542af
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-06-17 22:30:37 +0200

    17.06.2024

--- src/scripts/menuReact.mjs
+++ src/scripts/menuReact.mjs
@@ -23,6 +23,30 @@ const menuItem = [
     name: '04. Formularze',
     link: '../htmlReact/04-formularze.html',
   },
+  {
+    name: '05. Cykl życia',
+    link: '../htmlReact/05-cyklZyciowy.html',
+  },
+  {
+    name: '06. Zapytania HTTP',
+    link: '../htmlReact/06-zapytaniaHttp.html',
+  },
+  {
+    name: '07. React hooks',
+    link: '../htmlReact/07-reactHooks.html',
+  },
+  {
+    name: '08. Kontekst i referencje',
+    link: '../htmlReact/08-kontekstReferencje.html',
+  },
+  {
+    name: '09. Nawigacja',
+    link: '../htmlReact/09-nawigacja.html',
+  },
+  {
+    name: '10. Nawigacja2',
+    link: '../htmlReact/10-nawigacja2.html',
+  },
 ];
 
 menuItem.forEach(element => {
@@ -45,3 +69,23 @@ menuItem.forEach(element => {
     list.appendChild(listItem);
   }
 });
+
+//Footer
+const footerLogo = document.querySelector('div.footer-logo');
+const animationIcon = document.querySelector('div#footerAnimationIcon');
+const egzample = document.querySelector('div.footer-date');
+footerLogo.addEventListener('mouseover', beginAnimation, false);
+footerLogo.addEventListener('mouseout', endAnimation, false);
+
+function beginAnimation() {
+  // animationIcon.classList.toggle('footer-hidden');
+  animationIcon.classList.remove('footer-hidden');
+  animationIcon.classList.add('footer-animation');
+  egzample.style.color = '#f16c6c';
+}
+
+function endAnimation() {
+  // animationIcon.classList.toggle('footer-hidden');
+  animationIcon.classList.add('footer-hidden');
+  animationIcon.classList.remove('footer-animation');
+}
